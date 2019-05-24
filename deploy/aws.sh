f2599f444e8ba6ce934bbab46b66bddd3f353de5
Volodymyr.Palii <palii.volodymyr@apriorit.com>
2019-05-24 12:06:42 +0300

    Fix AWS ECS cluster name

--- deploy/aws.sh
+++ deploy/aws.sh
@@ -11,4 +11,5 @@ docker push 866680356172.dkr.ecr.us-east-2.amazonaws.com/blockexplorer-db
 docker push 866680356172.dkr.ecr.us-east-2.amazonaws.com/blockexplorer-sync:$TAG
 docker push 866680356172.dkr.ecr.us-east-2.amazonaws.com/blockexplorer-api:$TAG
 docker push 866680356172.dkr.ecr.us-east-2.amazonaws.com/blockexplorer-nginx:$TAG
-aws ecs update-service --service blockexplorer --cluster blockexplorer-$TAG --region us-east-2 --force-new-deployment
+if [ $TAG == "main" ] ; then CLUSTER=blockexplorer-prod ; else CLUSTER=blockexplorer-preprod ; fi
+aws ecs update-service --service blockexplorer --cluster $CLUSTER --region us-east-2 --force-new-deployment
