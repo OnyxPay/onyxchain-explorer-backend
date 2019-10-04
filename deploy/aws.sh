3b52375de6d4fbf01b7b000cb7c31f54d3169803
Volodymyr Palii <palii.volodymyr@apriorit.com>
2019-10-04 23:18:43 +0300

    Update Travis config (ONXP-1460)

--- deploy/aws.sh
+++ deploy/aws.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-TAG=$1
 echo "install awscli"
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
@@ -11,7 +10,5 @@ for i in ontsynhandler explorer holder statistics ; do
     docker push 866680356172.dkr.ecr.us-east-2.amazonaws.com/blockexplorer-v2-$i ;
 done
 
-if [ $TAG == "main" ] ; then
-    aws ecs update-service --cluster blockexplorer-v2-main --service blockexplorer-v2 --region us-east-2 --force-new-deployment ;
-    aws ecs update-service --cluster blockexplorer-v2-test --service blockexplorer-v2-test --region us-east-2 --force-new-deployment ;
-fi
+aws ecs update-service --cluster blockexplorer-v2-main --service blockexplorer-v2 --region us-east-2 --force-new-deployment ;
+aws ecs update-service --cluster blockexplorer-v2-test --service blockexplorer-v2-test --region us-east-2 --force-new-deployment ;
