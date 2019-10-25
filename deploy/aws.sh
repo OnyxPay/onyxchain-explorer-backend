e0e91e533800a9f25f2cb0f3cf617ad7b9c5b9b0
Volodymyr Palii <palii.volodymyr@apriorit.com>
2019-10-25 11:19:06 +0300

    Workaround for build fails (ONXP-1462)

--- deploy/aws.sh
+++ deploy/aws.sh
@@ -6,9 +6,7 @@ export PATH=$PATH:$HOME/.local/bin
 
 echo "ecr login"
 eval $(aws ecr get-login --region us-east-2 --no-include-email)
-for i in ontsynhandler explorer holder statistics ; do
-    docker push 866680356172.dkr.ecr.us-east-2.amazonaws.com/blockexplorer-v2-$i ;
-done
+docker-compose push ontsynhandler explorer statistics # TODO: ONXP-1650 Fix holder build
 
-aws ecs update-service --cluster blockexplorer-v2-main --service blockexplorer-v2 --region us-east-2 --force-new-deployment ;
-aws ecs update-service --cluster blockexplorer-v2-test --service blockexplorer-v2-test --region us-east-2 --force-new-deployment ;
+aws ecs update-service --cluster blockexplorer-v2-main --service blockexplorer-v2 --region us-east-2 --force-new-deployment
+aws ecs update-service --cluster blockexplorer-v2-test --service blockexplorer-v2-test --region us-east-2 --force-new-deployment
